5bc31533e76706b6ead62f5cd71b91479aa8240c
JoySmarty <smartyjoy47@gmail.com>
2022-06-12 12:51:45 +0100

    my first stopwatch

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+//BEGIN STOPWATCH PROGRAM
+const stopwatchDisplay=document.querySelector('.stopwatch-display')
+const stopwatchStartBtn=document.querySelector('.btn-stopwatch-Start')
+const stopwatchStopBtn=document.querySelector('.btn-stopwatch-Stop')
+const stopwatchResetBtn=document.querySelector('.btn-stopwatch-Reset')
